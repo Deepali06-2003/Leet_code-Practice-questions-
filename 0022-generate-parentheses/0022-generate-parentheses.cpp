5f34f819313795e7870dd6b6debdc8a0f96a0c31
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-04 12:23:34 +0530

    Time: 3 ms (72.14%), Space: 16.4 MB (14.37%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+void result(int n , vector<string>&res , string curr , int open , int close ){
+    if(curr.size() == 2*n){
+        res.push_back(curr);
+        return;
+    }
+    
+    if(open<n){
+        curr.push_back('(');
+        result(n , res , curr , open+1 , close );
+        curr.pop_back();
+        
+    }
+    if(open>close){
+        curr.push_back(')');
+        result(n , res , curr ,open , close+1 );
+       curr.pop_back();
+    }
+    
+}
+
+    vector<string> generateParenthesis(int n) {
+
+     
+     vector<string> res;
+     string curr;
+
+     result(n, res , curr , 0 , 0 );   
+     return res;
+    }
+};
