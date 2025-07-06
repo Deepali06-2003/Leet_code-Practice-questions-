43a3bd88293d31f6f4801bb075befd4cbd4475f6
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-07 00:21:31 +0530

    Time: 0 ms (100%), Space: 40.3 MB (58.25%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,7 +4,7 @@ public:
        int n = nums.size();
     vector<int> result(n, 1);
     
-    int prefix = 1;
+     int prefix = 1;
     for (int i = 0; i < n; i++) {
         result[i] = prefix;
         prefix *= nums[i];
