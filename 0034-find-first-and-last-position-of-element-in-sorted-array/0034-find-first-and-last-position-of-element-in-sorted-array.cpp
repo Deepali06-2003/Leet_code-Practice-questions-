7f6731b1d627e6364e16e45082207008a1896686
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-09 00:47:04 +0530

    Time: 0 ms (100%), Space: 18.1 MB (17.51%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,59 +1,50 @@
 class Solution {
 public:
-int f_occ(vector<int>&nums , int target){
-    int ans = -1;
-        int l = 0;
-        int h = nums.size()-1;
-        while(l<=h){
-            int m = (l+h)/2;
-            
-            if(nums[m]==target){
-                ans = m;
-                h = m-1;
-                
-            }
-            else if(target >nums[m]){
-                l = m+1;
-            }
-            else{
-                h = m-1;
-            }
-        }
-        return ans;
-}
-int l_occ(vector<int>&nums, int target){
-    int ans = -1;
-        int l = 0;
-        int h = nums.size()-1;
-        while(l<=h){
-            int m = (l+h)/2;
-            
-            if(nums[m]==target){
-                ans = m;
-                l = m+1;
-                
-            }
-            else if(target >nums[m]){
-                l = m+1;
-            }
-            else{
-                h = m-1;
-            }
-        }
-        return ans;
-}
+int f_occ(vector<int> nums , int t){
+    int l =0;
+    int h = nums.size()-1;
+    int ans=-1;
+    while(l<=h){
+        int m = (l+h)/2;
 
-
-    vector<int> searchRange(vector<int>& nums, int target) {
-        vector <int>ans(2 , -1);
-        if(nums.size()==0){
-            return ans;
+        if(nums[m] == t){
+            ans = m;
+            h = m-1;
         }
-        ans[0] = (f_occ(nums , target));
-        ans[1] = (l_occ(nums , target));
-        return ans;
-
+        else if(nums[m]> t){
+            h = m-1;
+        }
+        else{
+            l = m+1;
+        }
     }
-    
+    return ans;
+}
 
+int l_occ(vector<int> nums , int t){
+    int l =0;
+    int h = nums.size()-1;
+    int ans=-1;
+    while(l<=h){
+        int m = (l+h)/2;
+
+        if(nums[m] == t){
+            ans = m;
+            l = m+1;
+        }
+        else if(nums[m]> t){
+            h = m-1;
+        }
+        else{
+            l = m+1;
+        }
+    }
+    return ans;
+}
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> res(2 , -1);
+        res[0]= f_occ(nums , target);
+        res[1]= l_occ(nums , target);
+        return res;
+    }
 };
