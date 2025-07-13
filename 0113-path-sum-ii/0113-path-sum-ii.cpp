07ff6fe22b2f7770c4a7167a20492f55c7e38813
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-13 23:43:48 +0530

    Time: 4 ms (43.68%), Space: 20.9 MB (88.59%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -25,11 +25,11 @@ void helper(TreeNode* root , int target , vector<vector<int>>& res , vector<int>
             res.push_back(curr);
         }
     }
-    else{
+    //else{
         helper(root->left , target , res, curr , x);
         helper(root->right , target , res , curr , x);
         
-    }
+    //}
     curr.pop_back();
         
     
@@ -42,8 +42,8 @@ void helper(TreeNode* root , int target , vector<vector<int>>& res , vector<int>
         if(root== NULL && 0 == targetSum){
             return res;
         }
-        int x=0;
-        helper(root , targetSum , res , curr , x);
+        
+        helper(root , targetSum , res , curr , 0);
 
         return res;
         
