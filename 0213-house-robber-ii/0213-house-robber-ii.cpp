bd5a54123c21a6677687cd80ea750038a0c1e1d5
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-15 00:36:09 +0530

    Time: 0 ms (100%), Space: 10.9 MB (39.95%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -2,28 +2,25 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-
         if(n==0)return 0;
-        if(n==1) return nums[0];
+        if(n==1)return nums[0];
 
-        vector<int>dp1(n ,0);
-        vector<int>dp2(n,0);
+        vector<int>arr(n , 0);
+        vector<int>arr2(n,0);
+        arr[0] = nums[0];
+        arr[1] = max(nums[0] , nums[1]);
 
-        dp1[0]= 0;
-        dp1[1]= nums[1];
+        for(int i=2 ;i<n;i++){
+            
+            arr[i] = max( arr[i-1] , arr[i-2] + nums[i]);
+        }
+        arr2[0]=0;
+        arr2[1]= nums[1];
         for(int i=2;i<n;i++){
-            dp1[i] = max(dp1[i-1] , dp1[i-2] + nums[i]);
+            if(i== (n-1))arr[i]= arr[i-1];
+            arr2[i]= max(arr2[i-1] , arr2[i-2]+nums[i]);
         }
 
-        dp2[0]= nums[0];
-        dp2[1]= max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            if(i == n-1)dp2[i]= dp2[i-1];
-            else{
-                dp2[i] = max(dp2[i-1] , dp2[i-2]+nums[i]);
-            }
-        }
-
-        return max(dp1.back(), dp2.back());
+        return max(arr.back() , arr2.back());
     }
 };
