df5a256191eb6570cb3f157d1755bc8fcf7f99e8
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-23 10:01:54 +0530

    Time: 0 ms (100%), Space: 8.9 MB (19.8%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char>st;
+
+        for(int i =0;i<s.size();i++){
+            if(s[i] == '(' || s[i]=='{' || s[i] == '['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty())
+                return false;
+                if(!st.empty()){
+                    char x = st.top();
+                    if( (x=='(' && s[i]==')') || ( x=='{' && s[i]=='}')  ||  (x=='[' && s[i]==']')) 
+                    st.pop();
+                    else 
+                    return false;
+                }
+                
+            }
+        }
+        if(st.empty())return true;
+
+        return false;
+    }
+};
