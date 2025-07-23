ffb818ce9eb6590aa2c170b733ed5ccb0fdee2e5
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-23 19:35:29 +0530

    Time: 0 ms (100%), Space: 10.3 MB (76.38%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         stack<int> st;
-    st.push(0);
+    st.push(-1);
     int max_len = 0;
 
     for (int i = 0; i < s.size(); i++) {
