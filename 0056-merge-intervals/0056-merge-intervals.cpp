1654fc49be8ca0786c119b277138d944f4ca705c
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-24 09:50:47 +0530

    Time: 5 ms (55.57%), Space: 24 MB (23.17%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,14 +4,18 @@ public:
         int n=intervals.size();
         vector<vector<int>> result;
         sort(intervals.begin(),intervals.end());
-        result.push_back(intervals[0]);
+        //result.push_back(intervals[0]);
+        int start = intervals[0][0];
+        int end = intervals[0][1];
         for(int i=1; i<n; i++){
-            if(intervals[i][0]<=result.back()[1]){
-                result.back()[1]=max(intervals[i][1],result.back()[1]);
-            }else{
-                result.push_back(intervals[i]);
+            if(intervals[i][0] <= end)end = max(end , intervals[i][1]);
+            else{
+                result.push_back({start , end});
+                start = intervals[i][0];
+                end = intervals[i][1];
             }
         }
+        result.push_back({start , end});
         return result;
     }
 };
