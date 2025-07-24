966e6845a9d78acf6486f6cd94b163e9b3514203
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-24 10:29:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        vector<int>n(2 , 0);
-   
+        int n = nums.size();
+        unordered_map<int, int>mp;
 
-        for(int i =0;i<nums.size();i++){
+        for(int i =0;i<n;i++){
+            int nx = target - nums[i];
 
-            for(int j =i+1 ;j<nums.size();j++){
-                if(nums[i]+nums[j] == target){
-                    n[0] = i;
-                    n[1] = j;
-                    return n;
-                }
-            }
+            if(mp.count(nx))return {mp[nx], i};
+
+            mp[nx]=i;
         }
-        return n;
+        return {-1, -1};
     }
 };
