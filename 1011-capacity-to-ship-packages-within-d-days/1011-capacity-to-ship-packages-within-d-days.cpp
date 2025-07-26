e47981b4615f9fc3f9224d52b5fb055f9c81e434
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-26 21:51:54 +0530

    Time: 15 ms (34.17%), Space: 35.1 MB (34.7%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-bool helper(vector<int>& weights, int days , int m){
-    int c=0;
-    int d=1;
-    for(int i =0;i<weights.size();i++){
-
-        if(c+ weights[i] > m){
-            d++;
-            c=0;
+bool helper(vector<int>& weights, int days, int m ){
+    int c =1;
+    int curr=0;
+    for(int i =0;i<weights.size() ;i++){
+        if(curr+ weights[i] >m){
+            c++;
+            curr=0;
         }
-        c= c+ weights[i];
+        curr += weights[i];
     }
-    return d<= days;
+    return c<= days;
 }
     int shipWithinDays(vector<int>& weights, int days) {
         int l = *max_element(weights.begin() , weights.end());
         int h = accumulate(weights.begin() , weights.end() , 0);
 
         while(l<h){
+
             int m = (l+h)/2;
 
-            if(helper(weights , days , m)){
-                h=m;
-            }
+            if(helper(weights , days , m)) h = m;
+
             else{
                 l = m+1;
             }
         }
+
         return h;
     }
 };
