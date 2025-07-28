763dea471759f3150db6062f434bf78c0b403388
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-28 23:29:41 +0530

    Time: 9 ms (41.71%), Space: 53.1 MB (55%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -12,12 +12,15 @@ public:
                 i++;
             }
         }
-vector <int> arr;
-for(int j =0;j<n ;j++){
-            if(nums[j]-1 != j){
-                arr.push_back(j+1);
+
+        vector<int>res;
+
+        for(int j =0; j<nums.size() ;j++){
+            if(nums[j] != j+1){
+                res.push_back(j+1);
             }
         }
-        return arr;
+        return res;
     }
+
 };
