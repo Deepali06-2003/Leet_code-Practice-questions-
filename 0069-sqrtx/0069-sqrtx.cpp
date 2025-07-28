b06f65212ad44e392b1680bcefc0b007fda24b0d
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-28 19:02:59 +0530

    Time: 0 ms (100%), Space: 8.6 MB (50.89%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int mySqrt(int x) {
+
         int l = 0;
-        long long h = x;
+        int h = x;
         int ans= -1;
         while(l<=h){
             int m = (l+h)/2;
 
-            if((long long)m*m == x){
-                return m;
-            }
-            else if((long long)m*m <x){
+            if((long long)m* m == x)return m;
+
+            else if( (long long)m* m < x){
                 ans = m;
-                l= m+1;
+                l = m+1;
             }
             else{
                 h = m-1;
             }
         }
         return ans;
+        
     }
 };
