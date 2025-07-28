9aac7d578b46cc4c9e6f706cc9a6d29b084d5703
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-28 19:09:02 +0530

    Time: 0 ms (100%), Space: 17.3 MB (99.32%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,54 @@
 class Solution {
 public:
-int f_occ(vector<int> nums , int t){
-    int l =0;
+int f_occ(vector<int>& nums, int target){
+    int l = 0;
     int h = nums.size()-1;
     int ans=-1;
     while(l<=h){
         int m = (l+h)/2;
 
-        if(nums[m] == t){
+        if(nums[m] == target){
             ans = m;
             h = m-1;
         }
-        else if(nums[m]> t){
-            h = m-1;
+        else if(nums[m]<target){
+            l=m+1;
         }
         else{
-            l = m+1;
+            h = m-1;
         }
     }
     return ans;
 }
 
-int l_occ(vector<int> nums , int t){
-    int l =0;
+int l_occ(vector<int>& nums, int target){
+    int l = 0;
     int h = nums.size()-1;
     int ans=-1;
     while(l<=h){
         int m = (l+h)/2;
 
-        if(nums[m] == t){
+        if(nums[m] == target){
             ans = m;
             l = m+1;
         }
-        else if(nums[m]> t){
-            h = m-1;
+        else if(nums[m]<target){
+            l=m+1;
         }
         else{
-            l = m+1;
+            h = m-1;
         }
     }
     return ans;
 }
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> res(2 , -1);
-        res[0]= f_occ(nums , target);
-        res[1]= l_occ(nums , target);
+        
+        vector<int> res(2);
+        int x1= f_occ(nums , target);
+        int x2 = l_occ(nums, target);
+        res[0] = x1;
+        res[1] = x2;
         return res;
     }
 };
