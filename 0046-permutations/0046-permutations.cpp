4ad3534f769acf04b3837f21b753812ef97b4d71
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-29 00:15:07 +0530

    Time: 0 ms (100%), Space: 10.6 MB (47.46%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-void result(vector<int>&nums , vector<vector<int>>& res , vector<int>& curr , int start){
-
-if(nums.size() == curr.size()){
-    res.push_back(curr);
-    return;
-    
-}
-for(int i= 0 ;i<nums.size();i++){
-    if(find(curr.begin() , curr.end() , nums[i] ) != curr.end()){
-        continue;
+void helper(vector<int>& nums, vector<int>&curr ,vector<vector<int>>&res , int s){
+    if(curr.size() == nums.size()){
+        res.push_back(curr);
+        return;
     }
-    curr.push_back(nums[i]);
-    result(nums , res , curr , i+1);
-    curr.pop_back();
+    for(int i =0 ;i<nums.size();i++){
+        if( find( curr.begin() , curr.end() , nums[i]) != curr.end())continue;
+
+        curr.push_back(nums[i]);
+        helper(nums , curr, res  , i+1);
+        curr.pop_back();
+    }
 
 }
-}
-
-
     vector<vector<int>> permute(vector<int>& nums) {
-        
-        vector<vector<int>> res;
         vector<int>curr;
+        vector<vector<int>>res;
 
-        result(nums, res , curr , 0);
+        helper(nums , curr , res, 0);
         return res;
     }
 };
