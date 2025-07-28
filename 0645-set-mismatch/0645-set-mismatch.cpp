d570fa5ec81ede95cbe0f7c5552234fb14a028ba
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-28 23:18:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int i=0;
-        while(i<nums.size()){
+        int i = 0;
+        while(i < nums.size()){
+
             int c = nums[i]-1;
-            if(nums[i] != nums[c]){
-                swap(nums[i] , nums[c]);
+
+            if(nums[i]<nums.size() && nums[i] != nums[c]){
+                swap(nums[i], nums[c]);
             }
             else{
                 i++;
             }
         }
-vector<int>res(2, -1);
-        for(int j=0;j<nums.size();j++){
-            if(nums[j]-1 != j){
-                res[0]= nums[j];
-                res[1]= j+1;
+
+        vector<int>res;
+        for(int j=0;j<nums.size() ;j++){
+            if(nums[j] != j+1){
+                res.push_back(nums[j]);
+                res.push_back(j+1);
+                return res;
             }
         }
         return res;
