7ffd84b3d9b10702cee2c4b34436c80a1698ebe3
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-30 19:58:29 +0530

    Time: 2 ms (73.84%), Space: 102.1 MB (65.51%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-int helper(vector<vector<int>>& grid , int n , int m){
-    int ans=0;
-    for(int i=0;i<n ;i++){
-        for(int j =0;j<m ;j++){
-            if(grid[i][j] ==1){
-                if(i-1 < 0 || grid[i-1][j] ==0)ans++;
-                if(i+1 == n || grid[i+1][j] == 0) ans++;
-                if(j-1 <0 || grid[i][j-1] ==0)ans++;
-                if(j+1 == m || grid[i][j+1] ==0)ans++;
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int n = grid.size() , m = grid[0].size();
+        int p =0;
+
+        for(int i =0;i<n ;i++){
+            for(int j =0;j<m;j++){
+                if(grid[i][j] ==1){
+                    if(i-1 < 0 || grid[i-1][j] == 0) p++;
+                    if(i+1 == n || grid[i+1][j] == 0)p++;
+                    if( j-1 <0 || grid[i][j-1] ==0)p++;
+                    if( j+1 == m || grid[i][j+1] == 0 )p++;
+                }
             }
         }
-    }
-    return ans;
-}
-    int islandPerimeter(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        return helper(grid , n , m);
+        return p;
     }
 };
