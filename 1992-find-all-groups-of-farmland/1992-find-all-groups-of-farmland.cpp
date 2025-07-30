800560776d5a8b49688edafc405549b768875c6a
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-30 20:06:56 +0530

    Time: 30 ms (71.9%), Space: 64.7 MB (80.53%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& land, int i, int j, int n, int m, vector<int>& curr, int& max_i, int& max_j) {
-        if (i < 0 || j < 0 || i >= n || j >= m || land[i][j] == 0) return;
+   
+    void dfs(vector<vector<int>>& land , int i , int j , int n , int m , int& max_i , int& max_j){
+        if(i<0 || j<0 || i==n || j == m || land[i][j] == 0)return;
 
-        land[i][j] = 0;
-        max_i = max(max_i, i);
-        max_j = max(max_j, j);
+        land[i][j]=0;
+        max_i = max(max_i , i);
+        max_j = max(max_j , j);
 
-        dfs(land, i + 1, j, n, m, curr, max_i, max_j);
-        dfs(land, i, j + 1, n, m, curr, max_i, max_j);
+        dfs(land , i+1 , j , n , m , max_i , max_j);
+        dfs(land , i , j+1 , n , m , max_i , max_j);
     }
-
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
         int n = land.size();
         int m = land[0].size(); 
@@ -18,13 +18,16 @@ public:
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
+
                 if (land[i][j] == 1) {
-                    vector<int> curr = {i, j};
-                    int max_i = i, max_j = j;
-                    dfs(land, i, j, n, m, curr, max_i, max_j);
-                    curr.push_back(max_i);
-                    curr.push_back(max_j);
-                    res.push_back(curr);
+                    vector<int>curr(4);
+                    curr[0]=i;
+                    curr[1]=j;
+                    int max_i = i , max_j = j;
+                    dfs(land , i , j , n , m , max_i , max_j);
+                    curr[2]= max_i;
+                    curr[3] = max_j;
+                    res.push_back(curr); 
                 }
             }
         }
