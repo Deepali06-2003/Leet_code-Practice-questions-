383f335852398478f2ade39a07510156c706980a
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-31 00:15:07 +0530

    Time: 2 ms (83.49%), Space: 19.3 MB (63.88%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,45 +1,43 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj_list(numCourses);
-        
+        vector<vector<int>>adj(numCourses);
+
         for(int i=0;i<prerequisites.size();i++){
             int u = prerequisites[i][0];
             int v = prerequisites[i][1];
-            adj_list[u].push_back(v);
 
+            adj[u].push_back(v);
         }
 
-        vector<int>indegree(numCourses , 0);
-        for(int i =0; i<numCourses ;i++){
-            for(int j =0;j<adj_list[i].size();j++){
-                int x = adj_list[i][j];
+        vector<int>indegree(numCourses, 0);
+        for(int i =0;i<numCourses; i++){
+            for(int j =0;j<adj[i].size() ;j++){
+                int x = adj[i][j];
                 indegree[x]++;
             }
         }
 
         queue<int>q;
         vector<int>res;
-        
         for(int i =0;i<indegree.size();i++){
-            if(indegree[i]==0) q.push(i);
+            if(indegree[i]==0)
+            q.push(i);
         }
 
         while(!q.empty()){
-            int temp = q.front();
+
+            int t = q.front();
             q.pop();
-            res.push_back(temp);
+            res.push_back(t);
 
-            for(int i=0;i<adj_list[temp].size(); i++){
-
-                int x = adj_list[temp][i];
+            for(int i = 0 ; i< adj[t].size(); i++){
+                int x = adj[t][i];
                 indegree[x]--;
-                if(indegree[x]==0) q.push(x);
+                if(indegree[x]==0)q.push(x);
             }
         }
-
         if(res.size() == numCourses)return true;
-
         return false;
     }
 };
