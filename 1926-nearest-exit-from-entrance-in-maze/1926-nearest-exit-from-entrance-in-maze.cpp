332c268d655586000d1e61c3eda0ac7bbb6eddde
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-30 23:52:14 +0530

    Time: 2 ms (89.86%), Space: 34.9 MB (96.71%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -4,41 +4,39 @@ public:
         int n = maze.size();
         int m = maze[0].size();
 
-        queue<pair<int, int>>q;
-        q.push({entrance[0],entrance[1]});
-        maze[entrance[0]][entrance[1]] = '+';
+        queue<pair<int , int>>q;
+        int a = entrance[0];
+        int b = entrance[1];
+        maze[a][b] = '+';
+        q.push({a,b});
+
+        vector<int>dx = {0 , 0 , 1, -1};
+        vector<int>dy = {1 , -1 , 0 , 0};
 
         int c=0;
 
-        vector<int>dx={0 , 1, 0 , -1};
-        vector<int>dy ={1 , 0 , -1 , 0};
+        while(!q.empty()){
+            int l = q.size();
+            for(int i =0;i<l ;i++){
 
-        while(!q.empty()){  
-            int sz = q.size();
-
-            for(int i =0;i<sz;i++){
-
-                pair<int,int>ele = q.front();
+                pair<int,int>t = q.front();
                 q.pop();
-
-                int ele_i = ele.first;
-                int ele_j = ele.second;
+                int x = t.first;
+                int y = t.second;
 
                 for(int j =0;j<4;j++){
+                    int nx = x + dx[j];
+                    int ny = y+ dy[j];
 
-                    int nx = ele_i + dx[j];
-                    int ny = ele_j + dy[j];
-
-                    if(nx>=0 && nx< n && ny>=0 && ny<m && maze[nx][ny] =='.'){
-                        if (nx == 0 || ny == 0 || nx == n - 1 || ny == m - 1)return c + 1;
-                        maze[nx][ny] = '+';
+                    if(nx>=0 && nx<n && ny>=0 && ny<m && maze[nx][ny] == '.'){
                         q.push({nx , ny});
+                        maze[nx][ny] ='+';
+                        if(nx==0 || nx==n-1 || ny==0 || ny==m-1)return c+1;
                     }
-
                 }
-            }
-            c++;
+            }c++;
         }
         return -1;
+
     }
 };
