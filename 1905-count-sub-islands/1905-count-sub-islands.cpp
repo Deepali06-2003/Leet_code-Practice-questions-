72fc45d1443dafe04f7d09a9e493e18c1d7af1e7
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-30 20:15:36 +0530

    Time: 23 ms (90.3%), Space: 98.3 MB (54.49%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-void dfs(vector<vector<int>>& grid1 , vector<vector<int>>& grid2 , int i , int j , int n , int m , bool& isSubIsland){
-    if(i<0 || j<0 || i== n|| j==m || grid2[i][j]!=1)return;
+void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2 , int i , int j , int n , int m , bool& isbool){
 
-    if (grid1[i][j] != 1) isSubIsland = false;
-    grid2[i][j] = 0;
-    grid1[i][j] =0;
+    if(i<0 || j<0 || i== n|| j==m || grid2[i][j]==0)return;
 
-    dfs(grid1 , grid2 ,i+1 , j , n, m , isSubIsland);
-    dfs(grid1 ,grid2,i , j-1 , n , m, isSubIsland);
-    dfs(grid1 ,grid2,  i-1 , j , n , m, isSubIsland);
-    dfs(grid1 ,grid2,  i , j+1 , n, m, isSubIsland);
+    if(grid1[i][j]!=1)isbool = false;
+
+    grid2[i][j]=0;
+    grid1[i][j]=0;
+
+    dfs(grid1 , grid2 , i+1 , j , n , m , isbool);
+    dfs(grid1 , grid2 , i , j+1 , n , m , isbool);
+    dfs(grid1 , grid2 , i-1 , j , n , m , isbool);
+    dfs(grid1 , grid2 , i , j-1 , n , m , isbool);
+
 }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        int n = grid1.size();
-        int m = grid1[0].size();
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j =0 ;j<m;j++){
-                if(grid2[i][j] == 1){
-                    bool isSubIsland = true;
-                    dfs(grid1, grid2, i, j, n, m, isSubIsland);
-                    if (isSubIsland) count++;
+        int n = grid2.size();
+        int m = grid2[0].size();
+        int c=0;
+
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(grid2[i][j] ==1){
+                    bool isbool = true;
+                    dfs(grid1 , grid2 , i , j , n, m, isbool);
+                    if(isbool)c++;
                 }
             }
         }
-        
-        return count;
-        
+        return c;
     }
 };
