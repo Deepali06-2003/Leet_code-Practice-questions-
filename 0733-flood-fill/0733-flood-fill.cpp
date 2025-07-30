38acb4012eb3cbb0f232175da10ea1fcdbe80abd
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-30 19:48:49 +0530

    Time: 0 ms (100%), Space: 17.8 MB (98.97%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-void DFS(vector<vector<int>>&image , int i , int j , int n , int m ,int old_c , int new_c){
-    if(i<0 || j<0 || i== n || j == m || image[i][j] == new_c || image[i][j] != old_c){
-        return;
-    }
+void dfs(vector<vector<int>>& image, int sr, int sc,int n , int m, int target, int color){
+    if(sr<0 || sc<0 || sr==n|| sc==m || image[sr][sc]== color || image[sr][sc] != target)return;
 
-    image[i][j] = new_c;
+    image[sr][sc] = color;
 
-    DFS(image , i+1 , j , n , m, old_c , new_c);
-    DFS(image , i , j-1 , n , m, old_c , new_c);
-    DFS(image , i-1 , j , n , m, old_c , new_c);
-    DFS(image , i , j+1 , n , m, old_c , new_c);
+    dfs(image , sr+1 , sc , n , m , target , color);
+    dfs(image , sr , sc+1 , n , m,target , color);
+    dfs(image , sr-1 , sc , n , m , target , color);
+    dfs(image , sr , sc-1 , n , m,target , color);
 
 }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-
         int n = image.size();
         int m = image[0].size();
 
-        int old_c = image[sr][sc];
-        DFS(image , sr , sc , n , m , old_c , color);
+        int target = image[sr][sc];
+
+        dfs(image , sr , sc ,n , m , target , color);
 
         return image;
-        
     }
 };
