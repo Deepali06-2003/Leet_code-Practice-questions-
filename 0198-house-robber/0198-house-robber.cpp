24de8ee773fe33628cbed1a3f82bae5fea263f13
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-30 17:57:09 +0530

    Time: 0 ms (100%), Space: 10.6 MB (64.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,16 +4,16 @@ public:
         int n = nums.size();
         if(n==0)return 0;
         if(n==1)return nums[0];
-        if(n==2)return max(nums[0] , nums[1]);
+        if(n==2) return max(nums[0] , nums[1]);
 
-        vector<int>arr(n);
-        arr[0] = nums[0];
-        arr[1] = max(nums[0] , nums[1]);
+        vector<int>dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0] , nums[1]);
 
-        for(int i=2 ;i<n;i++){
-            arr[i] = max( arr[i-1] , arr[i-2] + nums[i]);
+        for(int i =2 ;i< n;i++){
+            dp[i] = max(dp[i-2]+ nums[i] , dp[i-1]);
         }
 
-        return arr[n-1];
+        return dp[n-1];
     }
 };
