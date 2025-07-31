50e45aed73c85a03ef428e004aaa965f88ecb2ce
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-07-31 15:01:17 +0530

    Time: 16 ms (81.09%), Space: 26.2 MB (71.16%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
-if(arr.size()<3) return false;
-int i=1;
-int j=0;
-while(i<arr.size() && arr[i]>arr[j]){
-i++;j++;
-}
-if(i==1 || i>=arr.size()) return false;
 
-while(i<arr.size() && arr[i]<arr[j]){
-i++;j++;
-}
-return i==arr.size();
-}
+        if(arr.size()<3)return false;
+
+        int i =0;
+        while( i < arr.size() && ( arr[i] < arr[i+1] ) ){
+                i++;
+        }
+        if(i ==0 || i == arr.size()-1)return false;
+
+        for(int x = i+1 ; x<arr.size() ;x++){
+            if(arr[x] >= arr[x-1])return false;
+        }
+
+        //cout<<i;
+        return true;
+
+    };
 };
 
 
