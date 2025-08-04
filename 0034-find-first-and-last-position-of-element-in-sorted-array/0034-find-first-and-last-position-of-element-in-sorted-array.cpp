d179a9754089f77071c97e7d424bd337c8db1f52
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-08-04 20:02:54 +0530

    Time: 0 ms (100%), Space: 17.6 MB (17.2%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
 int f_occ(vector<int>& nums, int target){
-    int l = 0;
+    int l =0;
     int h = nums.size()-1;
-    int ans=-1;
+    int ans =-1;
+
     while(l<=h){
         int m = (l+h)/2;
-
         if(nums[m] == target){
             ans = m;
             h = m-1;
         }
-        else if(nums[m]<target){
-            l=m+1;
-        }
-        else{
-            h = m-1;
-        }
-    }
-    return ans;
-}
-
-int l_occ(vector<int>& nums, int target){
-    int l = 0;
-    int h = nums.size()-1;
-    int ans=-1;
-    while(l<=h){
-        int m = (l+h)/2;
-
-        if(nums[m] == target){
-            ans = m;
+        else if(nums[m] < target){
             l = m+1;
         }
-        else if(nums[m]<target){
-            l=m+1;
-        }
         else{
             h = m-1;
         }
@@ -42,13 +21,36 @@ int l_occ(vector<int>& nums, int target){
     return ans;
 }
 
+
+int l_occ(vector<int>& nums, int target){
+    int l =0;
+    int h = nums.size()-1;
+    int ans =-1;
+
+    while(l<=h){
+        int m = (l+h)/2;
+        if(nums[m] == target){
+            ans = m;
+            l= m+1;
+        }
+        else if(nums[m] < target){
+            l = m+1;
+        }
+        else{
+            h = m-1;
+        }
+    }
+    return ans;
+}
     vector<int> searchRange(vector<int>& nums, int target) {
         
-        vector<int> res(2);
-        int x1= f_occ(nums , target);
-        int x2 = l_occ(nums, target);
-        res[0] = x1;
-        res[1] = x2;
+        vector<int>res(2,0);
+        int x = f_occ(nums , target);
+        int y = l_occ(nums , target);
+
+        res[0] = x;
+        res[1] = y;
+
         return res;
     }
 };
