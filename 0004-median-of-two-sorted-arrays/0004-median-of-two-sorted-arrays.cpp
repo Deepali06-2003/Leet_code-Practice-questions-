2268781f9b1c6e16073f73dcadeaee63af347be2
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-08-06 01:07:50 +0530

    Time: 0 ms (100%), Space: 95.1 MB (60.41%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
-        int i = 0;
-        int j = 0;
 
-        int n = nums1.size();
-        int m = nums2.size();
-        vector<int>num(n+m , 0);
-        int k =0;
+        int n1 = nums1.size() , n2 = nums2.size();
+        if(n1 > n2) return findMedianSortedArrays(nums2 , nums1);
 
-        //merge and sorted num.
-        while(i< n && j <m){
-            if(nums1[i] <= nums2[j]){
-                num[k] = nums1[i];
-                k++;
-                i++;
+        int n = n1+n2;
+        int left = (n+1)/2;
+
+        int l = 0;
+        int h = n1;
+
+        while(l<= h){
+            int m1 = (l+h)/2;
+            int m2 = left - m1;
+
+            int l1 = INT_MIN , l2 = INT_MIN , r1 = INT_MAX , r2 = INT_MAX;
+
+            if(m1 < n1) r1 = nums1[m1];
+            if(m2 < n2) r2 = nums2[m2];
+
+            if(m1-1 >= 0) l1= nums1[m1-1];
+            if(m2-1 >= 0) l2 = nums2[m2-1];
+
+            if(l1 <= r2 && l2 <= r1){
+                if(n%2 != 0) return max(l1 , l2);
+                else return (double)(max(l1 , l2) + min(r1 , r2))/2.0;
             }
-            else{
-                num[k] = nums2[j];
-                k++;
-                j++;
-            }
-        }
-        while(i<n){
-            num[k] = nums1[i];
-            k++;
-            i++;
-        }
-        while(j<m){
-            num[k] = nums2[j];
-            k++;
-            j++;
-        }
 
-        //find if odd or even.
-        int x = n + m;
-        if (x % 2 == 0) return (num[x / 2 - 1] + num[x / 2]) / 2.0;
-       return num[x / 2];
-        
-        
+            else if(!(l1 < r2)) h = m1-1;
+
+            else if(!(l2 < r1)) l = m1+1;
+
+        }
+        return 0.0;
     }
 };
