391ad9bfb7d2d9e21211d814d0024cda7140dae7
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-08-20 21:50:00 +0530

    Time: 0 ms (100%), Space: 15 MB (61.19%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,34 +11,35 @@
  */
 class Solution {
 public:
-void helper(TreeNode* root , vector<int>& res , vector<int>&curr){
-    if(root== NULL) return ;
+void helper(TreeNode* root ,vector<int>& res, vector<int>& curr){
+    if(root == NULL)return;
 
     queue<TreeNode*>q;
     q.push(root);
-    while(!q.empty()){
-        curr.clear();
-        int l = q.size();
 
-        for(int i =0 ; i<l ;i++){
-            TreeNode* c = q.front();
+    while(!q.empty()){
+        int l  = q.size();
+        curr.clear();
+
+        for(int i =0;i<l;i++){
+            TreeNode* temp = q.front();
             q.pop();
 
-            curr.push_back(c->val);
-            
-            if(c->left) q.push(c->left);
-            if(c->right) q.push(c->right);
+            curr.push_back(temp->val);
+
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
         }
+
         res.push_back(curr.back());
-
     }
-    
-}
 
+}
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> res;
+        vector<int>res;
         vector<int>curr;
-        if(root == NULL) return res;
+
+        if(root == NULL)return res;
 
         helper(root , res , curr);
         return res;
