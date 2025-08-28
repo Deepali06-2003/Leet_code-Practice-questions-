bd12eeb81b13dc48c4082d3ecc58a28e6179b2b0
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-08-28 14:44:44 +0530

    Time: 0 ms (100%), Space: 10.2 MB (40.69%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-int solve(vector<int>& nums , int l , int h){
+int solve(vector<int>& nums , int l , int h, vector<vector<int>>& dp){
     if(l>h)return 0;
     if(l == h)return nums[l];
 
-    int i = nums[l] - solve(nums , l+1 , h);
-    int j = nums[h] - solve(nums , l , h-1);
+    if (dp[l][h] != INT_MIN) return dp[l][h];
 
-    return max(i , j);
+    int i = nums[l] - solve(nums , l+1 , h, dp);
+    int j = nums[h] - solve(nums , l , h-1, dp);
+
+    return dp[l][h] = max(i , j);
 }
     bool predictTheWinner(vector<int>& nums) {
-        return solve(nums , 0 , nums.size()-1) >=0;
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
+        return solve(nums , 0 , n-1 , dp) >=0;
     }
 };
