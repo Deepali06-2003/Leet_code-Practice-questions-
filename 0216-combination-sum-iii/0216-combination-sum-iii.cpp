e106446217f0359edf16b047fd3fa4bd4e60b791
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-08-28 23:44:54 +0530

    Time: 0 ms (100%), Space: 8.6 MB (80.32%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-void helper(int k , int n , int x ,int s, vector<int>& curr , vector<vector<int>>& res){
+    void helper(int k, int n, int x, int s, vector<int>& curr,
+                vector<vector<int>>& res) {
 
-    if(s == 0){
-        if(curr.size() == k){
-            res.push_back(curr);
+        if (s == 0) {
+            if (curr.size() == k) {
+                res.push_back(curr);
+            }
+            return;
         }
-        return;
-    }
 
-    for(int i = x ; i<= 9 ;i++){
-        if(i > s)break;
-        curr.push_back(i);
-        helper(k , n , i+1 , s- i , curr , res);
-        curr.pop_back();
+        for (int i = x; i <= 9; i++) {
+            if (i > s)
+                break;
+            curr.push_back(i);
+            helper(k, n, i + 1, s - i, curr, res);
+            curr.pop_back();
+        }
     }
-
-}
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> curr ;
-         vector<vector<int>> res;
+        vector<int> curr;
+        vector<vector<int>> res;
 
-        helper(k , n , 1 , n , curr , res);
+        helper(k, n, 1, n, curr, res);
         return res;
     }
 };
