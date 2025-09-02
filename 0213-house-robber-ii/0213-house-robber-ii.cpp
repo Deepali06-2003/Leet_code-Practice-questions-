c3312f2e8c190245c5395f189ef3dea793f093a2
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-09-02 18:10:40 +0530

    Time: 0 ms (100%), Space: 10.9 MB (40.78%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n =nums.size();
-        if(n==0)return 0;
-        if(n==1)return nums[0];
+        int n = nums.size();
 
-        vector<int>dp1(n);
-        vector<int>dp2(n);
+        if(n==1)return nums[0];
+        
+        vector<int>dp1(n, 0);
+        vector<int>dp2(n,0);
 
         dp1[0]= nums[0];
-        dp1[1] = max(nums[1] , nums[0]);
-        for(int i = 2;i<n;i++){
-            if(i == n-1){
-                dp1[i] = dp1[i-1];
-            }
-            else
-                dp1[i] = max(dp1[i-1] , dp1[i-2]+nums[i]);
+        dp1[1] = max(nums[0] , nums[1]);
+        for(int i =2;i<n;i++){
+            if(i == n-1)dp1[i] = dp1[i-1];
+            else dp1[i] = max(dp1[i-1] , dp1[i-2]+nums[i]);
         }
-        dp2[0] =0;
+
+        dp2[0] = 0;
         dp2[1] = nums[1];
-        for(int i = 2;i<n;i++){
+        for(int i =2;i<n;i++){
             dp2[i] = max(dp2[i-1] , dp2[i-2]+nums[i]);
         }
 
         return max(dp1[n-1] , dp2[n-1]);
-        
     }
 };
