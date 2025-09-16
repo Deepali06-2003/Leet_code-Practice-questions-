2247d70cb4a5467d0593eef008fd46752639b6ea
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-09-17 01:21:27 +0530

    Time: 0 ms (100%), Space: 15.3 MB (96.05%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -8,8 +8,8 @@ public:
         for(int i =0;i<n;i++){
             for(int j =0;j<m ;j++){
                 if(i ==0 && j==0) continue;
-                else if(i == 0 && j>0) grid[i][j] = grid[i][j-1]+ grid[i][j];
-                else if(j==0 && i>0) grid[i][j] = grid[i-1][j] +grid[i][j]; 
+                else if(i == 0) grid[i][j] = grid[i][j-1]+ grid[i][j];
+                else if(j==0) grid[i][j] = grid[i-1][j] +grid[i][j]; 
 
                 else{
                     grid[i][j] = min( grid[i-1][j] , grid[i][j-1])+grid[i][j];
