e50ffe9d1ae63cc7b68d5e703f305489cc2a78cc
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-12-06 00:32:00 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i =0;
-        for(int j =0; j<nums.size() ; j++){
-            if(nums[j] != 0){
-                swap(nums[i] , nums[j]);
-                i++;
+        int l=0;
+        
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != 0){
+                swap(nums[i] , nums[l]);
+                l++;
             }
         }
-        
     }
 };
