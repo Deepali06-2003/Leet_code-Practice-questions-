b8cd6ead2849a218b38d5f82046d3ef54c73d4e3
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-12-08 01:48:22 +0530

    Time: 17 ms (15.49%), Space: 35 MB (85.06%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
 int to_check(vector<int>& weights, int days, int m){
-    int s =0 , c_d =1;
+    int c_d =1 , s=0;
 
-    for(int i =0;i<weights.size();i++){
-        if((weights[i]+s) <= m) s = s+weights[i];
+    for(int i=0;i<weights.size();i++){
+        if((s+weights[i])<= m){
+            s = s+weights[i];
+        }
         else{
-            c_d = c_d+1;
+            c_d= c_d+1;
             s= weights[i];
         }
     }return days >= c_d;
+
+    
 }
     int shipWithinDays(vector<int>& weights, int days) {
-        
-        int ans = -1;
+        int ans =-1;
+
         int l = *max_element(weights.begin(), weights.end());
-        int h = accumulate(weights.begin() , weights.end(),0);
+        int h = accumulate(weights.begin(), weights.end(),0);
 
         while(l<=h){
             int m = (l+h)/2;
-            if(to_check(weights, days , m)){
+
+            if(to_check(weights , days , m)){
                 ans = m;
                 h = m-1;
             }
@@ -27,6 +32,5 @@ int to_check(vector<int>& weights, int days, int m){
                 l = m+1;
             }
         }return ans;
-
     }
 };
