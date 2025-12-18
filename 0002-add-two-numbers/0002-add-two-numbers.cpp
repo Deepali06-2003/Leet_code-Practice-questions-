85ee24536292f4af2a14ff0bf19546dfa839fb0b
Deepali Saroha <deepalisaroha2003@gmail.com>
2025-12-18 23:53:51 +0530

    Time: 3 ms (35.45%), Space: 77 MB (73.89%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,26 +11,34 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* d = new ListNode(0);
-        ListNode* t = d;
-        int s=0;
-        while(l1 || l2){
-            if(l1){
-                s = s+ l1->val;
-                l1 = l1->next;
+        ListNode* temph = new ListNode(0);
+        ListNode* temp = temph;
 
+        if(l1 == NULL && l2 == NULL)return NULL;
+        if(l1 == NULL)return l2;
+        if(l2 == NULL)return l1;
+
+        int x =0;
+
+        while(l1 != NULL || l2!= NULL){
+
+            if(l1 != NULL){
+                x = x+ l1->val;
+                l1 = l1->next;
             }
-            if(l2){
-                s= s+ l2->val;
+            if(l2 != NULL){
+                x= x+ l2->val;
                 l2 = l2->next;
             }
-            t->next = new ListNode(s%10);
-            s= s/10;
-            t=t->next;
+
+            temp->next = new ListNode(x%10);
+            x= x/10;
+            temp = temp->next;
         }
-        if(s!=0){
-            t->next = new ListNode(s%10);
+        if(x!= 0){
+            temp->next = new ListNode(x);
+            temp = temp->next;
         }
-        return d->next;
+        return temph->next;
     }
 };
