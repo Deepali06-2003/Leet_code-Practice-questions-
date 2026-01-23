bd78484b68ff2e0921325dc97ba016dfdda756d0
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-01-23 19:00:16 +0530

    Time: 10 ms (29.25%), Space: 55.1 MB (62.04%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -5,18 +5,14 @@ public:
 
         int i =0;
         while(i<n){
-
-            if(nums[i]>0 && nums[i]<n && nums[i]!= nums[nums[i]-1] ){
-                int curr = nums[i]-1;
-                swap(nums[i], nums[curr]);
-            }
-            else i++;
+            if(nums[i]<=n && nums[i]>0 && nums[i] != nums[nums[i] -1]){
+                int curr_i = nums[i]-1;
+                swap(nums[i], nums[curr_i]);
+            }else i++;
         }
 
-        for(int j=0;j<n;j++){
-            if(nums[j] != j+1){
-                return j+1;
-            }
+        for(int j =0;j<n;j++){
+            if(nums[j] != j+1)return j+1;
         }return i+1;
     }
 };
