40f8a9f02ef1be83c449516d3017196274a9e88f
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-01-23 18:34:44 +0530

    Time: 0 ms (100%), Space: 21.8 MB (68.97%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -5,19 +5,13 @@ public:
 
         int i =0;
         while(i<n){
-            int curr = nums[i];
-
-            if(nums[i]<n && nums[i]!=nums[curr]){
-                swap(nums[i], nums[curr]);
-            }else{
-                i++;
-            }
+            int curr_i = nums[i];
+            if(nums[i]<n && nums[i] != nums[curr_i])swap(nums[i], nums[curr_i]);
+            else i++;
         }
 
-        for(int j =0;j<n;j++){
-            if(nums[j] != j){
-                return j;
-            }
+        for(int j=0;j<n;j++){
+            if(nums[j] != j)return j;
         }return i;
     }
 };
