cfc3a0e54ee9371f57849488b463b66d514c7af1
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-01-24 15:12:14 +0530

    Time: 78 ms (16.6%), Space: 35 MB (15.16%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        
         int n = nums.size();
-        vector<vector<int>> res;
-
-        if(n<3)return res;
-
+        vector<vector<int>>res;
         sort(nums.begin(), nums.end());
 
-        for(int i=0;i<n-2;i++){
-            int target = -nums[i];
+        for(int i =0;i<n-2;i++){
+            int t = -nums[i];
 
-            int j =i+1, k = n-1;
+            int j = i+1;
+            int k = n-1;
 
-            if(i>0 && nums[i]== nums[i-1]){
-                continue;
-            }
+            if(i>0 && nums[i]==nums[i-1])continue;
 
             while(j<k){
-                if(nums[j]+nums[k] == target){
+                if(nums[k]+nums[j] ==t ){
 
                     vector<int>curr;
                     curr.push_back(nums[i]);
                     curr.push_back(nums[j]);
                     curr.push_back(nums[k]);
-                    j++;k--;
-                    
+
+                    j++;
+                    k--;
+
                     while(j<k && nums[j]==nums[j-1])j++;
                     while(j<k && nums[k]==nums[k+1])k--;
 
-                   res.push_back(curr);
+                    res.push_back(curr);
                 }
-                else if(nums[j]+nums[k] > target)k--;
+                else if(nums[k]+nums[j]>t )k--;
                 else j++;
-
             }
-            
-        }
-        return res;
+        }return res;
     }
 };
