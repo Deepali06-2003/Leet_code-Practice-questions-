dc09890d9ff7f8cb8a57c37e1948ff75bea5ba7d
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-01-24 10:05:27 +0530

    Time: 0 ms (100%), Space: 9.6 MB (78.58%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -4,25 +4,12 @@ public:
         
         int n = nums.size();
 
-        vector<vector<int>>dp(n+1, vector<int>(n+1 , 0));
-
-        for(int i=1;i<=n;i++){
-            for(int j =1;j<=n;j++){
-                if(i<j && nums[i-1] == nums[j-1]){
-                    //dp[i][j] = max(dp[i-1][j] , dp[i][j-1])+ 1;
-                    dp[i][j]=1;
-                }
-                
+        int c=0 , j =1;
+        while(j<n){
+            for(int i =0 ;i<j;i++){
+                if(nums[i] == nums[j])c++;
             }
-        }int c=0;
-        for(int i=0;i<=n;i++){
-            for(int j =0;j<=n;j++){
-                
-                    if(dp[i][j]==1)c++;
-                
-            }
-            cout<<endl;
-        }
-        return c;
+            j++;
+        }return c;
     }
 };
