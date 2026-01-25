c24d3e3f6f6d03506ebf2780218fdcb8fe173fde
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-01-25 19:50:13 +0530

    Time: 0 ms (100%), Space: 11.2 MB (56.49%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -11,15 +11,14 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        if(head == NULL || head->next == NULL){
-            return head;
-        }
-
-        ListNode* temp  = head->next;
+        
+        if(head== NULL || head->next == NULL)return head;
+               ListNode* temp = head->next;
 
         head->next = swapPairs(head->next->next);
         temp->next = head;
 
         return temp;
+        
     }
 };
