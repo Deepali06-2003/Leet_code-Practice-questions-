b303899de63ed41a343d9544df3dbdcf6405be4e
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-01-27 13:33:07 +0530

    Time: 10 ms (36.21%), Space: 13.3 MB (31.15%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
+        
         int n = word1.size();
         int m = word2.size();
 
-        vector<vector<int>> arr(n+1 , vector<int>(m+1 , 0));
-        for(int i=1;i<=n;i++)arr[i][0]= i;
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
 
-        for(int i=1;i<=m;i++)arr[0][i]= i;
+        for(int i=1;i<=n;i++) dp[i][0] =i;
+        for(int i=1;i<=m;i++) dp[0][i] =i;
 
-        for(int i=1;i<=n;i++){
-            for(int j = 1;j<=m;j++){
-                if(word1[i-1] == word2[j-1]){
-                    arr[i][j]= arr[i-1][j-1];
-                }
+        for(int i =1;i<=n;i++){
+            for(int j =1;j<=m;j++){
+
+                if(word1[i-1]==word2[j-1])dp[i][j]= dp[i-1][j-1];
                 else{
-                    arr[i][j] = 1 + min( { arr[i - 1][j - 1],  arr[i - 1][j], arr[i][j - 1] } ); 
+                    dp[i][j] = min( {dp[i-1][j-1], dp[i-1][j], dp[i][j-1] }) +1;
                 }
             }
         }
-        return arr[n][m];
+        return dp[n][m];
     }
 };
