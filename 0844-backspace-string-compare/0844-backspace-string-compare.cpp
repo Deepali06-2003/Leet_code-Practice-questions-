a4ec2b0c0ca9a3a802c70497648e5ecc66c4bf48
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-01-30 00:13:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char>s1;
+        stack<char>s2;
+
+        for(char i:s){
+            if(i=='#'){
+                s1.pop();
+            }else{
+                s1.push(i);
+            }
+        }
+
+        for(char i:t){
+            if(i=='#'){
+                s2.pop();
+            }else{
+                s2.push(i);
+            }
+        }
+
+        return s1==s2;
+    }
+};
