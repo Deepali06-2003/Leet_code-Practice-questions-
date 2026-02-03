c6d54cbde5a1738a336d660f9e79ffa409768305
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-04 00:41:20 +0530

    Time: 22 ms (89.99%), Space: 95.2 MB (76.17%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2 , int i , int j , int n , int m , bool& isbool){
 
-    if(i<0 || j<0 || i== n|| j==m || grid2[i][j]==0)return;
+void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2 , int i , int j , int n , int m, bool& isIland){
 
-    if(grid1[i][j]!=1)isbool = false;
+    if(i<0 || j<0 || i>=n || j>=m || grid2[i][j]!=1)return;
 
-    grid2[i][j]=0;
-    grid1[i][j]=0;
+    if(grid1[i][j] == 0)isIland = false;
 
-    dfs(grid1 , grid2 , i+1 , j , n , m , isbool);
-    dfs(grid1 , grid2 , i , j+1 , n , m , isbool);
-    dfs(grid1 , grid2 , i-1 , j , n , m , isbool);
-    dfs(grid1 , grid2 , i , j-1 , n , m , isbool);
+    grid1[i][j] = grid2[i][j] =0;
+
+    dfs(grid1 , grid2 , i+1 , j , n , m, isIland);
+    dfs(grid1 , grid2 , i-1 , j , n , m, isIland);
+    dfs(grid1 , grid2 , i , j+1 , n , m, isIland);
+    dfs(grid1 , grid2 , i , j-1 , n , m, isIland);
 
 }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         int n = grid2.size();
         int m = grid2[0].size();
-        int c=0;
+        int ans=0;
 
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(grid2[i][j] ==1){
-                    bool isbool = true;
-                    dfs(grid1 , grid2 , i , j , n, m, isbool);
-                    if(isbool)c++;
+                if(grid2[i][j]==1){
+                    bool isIland = true;
+                    dfs(grid1, grid2 , i , j ,n, m, isIland);
+                    if(isIland == true)ans++;
                 }
             }
         }
-        return c;
+        return ans;
     }
 };
