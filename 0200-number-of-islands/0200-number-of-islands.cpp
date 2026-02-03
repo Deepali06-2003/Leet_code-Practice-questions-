6011e3876a74f091ae8a02dc0bab6e27423c6870
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-04 00:03:07 +0530

    Time: 22 ms (88.03%), Space: 16.3 MB (78.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-void dfs(vector<vector<char>>& grid , int i , int j , int n , int m ){
-    if(i<0 || j<0 || i==n || j == m || grid[i][j] == '0')return;
+void helper(vector<vector<char>>& grid, int n , int m , int i , int j){
+    if(i<0 || j<0 || i==n || j==m || grid[i][j]=='0')return;
 
-    grid[i][j] ='0';
+    grid[i][j]='0';
 
-    dfs(grid , i+1 , j , n , m);
-    dfs(grid , i , j+1 , n , m);
-    dfs(grid , i-1 , j , n , m);
-    dfs(grid , i , j-1 , n , m);
+    helper(grid, n , m, i+1,j);
+    helper(grid, n , m, i-1,j);
+    helper(grid, n , m, i,j+1);
+    helper(grid, n , m, i,j-1);
 }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int ans =0;
-        for(int i =0;i<n;i++){
+
+        int ans=0;
+        for(int i=0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(grid[i][j] == '1'){
-                    dfs(grid , i , j , n , m);
+                if(grid[i][j]=='1'){
+                    helper(grid, n, m, i, j);
                     ans++;
                 }
             }
-        }
-        return ans;
+        }return ans;
     }
 };
