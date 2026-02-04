59264572b79cabb74ee75b62b5a57c00c9b575cb
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-05 01:04:05 +0530

    Time: 0 ms (100%), Space: 16.7 MB (72.91%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -4,50 +4,47 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        queue<pair<int,int>>q;
-        int f=0 , r=0;
-        for(int i =0;i<n;i++){
-            for(int j = 0;j<m;j++){
+        queue<pair<int , int>>q;
+        int f=0, r=0;
 
-                if(grid[i][j]== 1){
-                    f++;
-                }
-                else if(grid[i][j] == 2){
-                    q.push({i, j});
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1)f++;
+                else if(grid[i][j]==2){
                     r++;
+                    q.push({i,j});
                 }
             }
         }
+
         if(f==0)return 0;
         int ans=0;
 
-        vector<int>dx= {0 , 0 , 1 , -1};
-        vector<int>dy= {1 , -1 , 0 , 0};
+        vector<int>dx ={0 , 0 , 1, -1};
+        vector<int>dy ={1, -1, 0 , 0};
 
         while(!q.empty()){
-            int l = q.size();
-            for(int i =0 ;i<l ;i++){
-                pair<int, int>t = q.front();
+            int s = q.size();
+            
+            for(int i=0;i<s;i++){
+                pair<int, int>temp = q.front();
                 q.pop();
 
-                int x = t.first;
-                int y = t.second; 
+                for(int j=0;j<4;j++){
+                    int nx = temp.first +dx[j];
+                    int ny = temp.second + dy[j];
 
-                for(int j =0 ; j<4 ;j++){
-                    int nx = x+dx[j];
-                    int ny = y +dy[j];
-
-                    if(nx>=0 && nx< n && ny>=0 && ny<m && grid[nx][ny]==1){
+                    if(nx>=0 && nx<n && ny>=0 && ny<m && grid[nx][ny]==1){
                         grid[nx][ny]=2;
-                        q.push({nx , ny});
                         f--;
+                        q.push({nx, ny});
                     }
                 }
             }
             if(!q.empty())ans++;
         }
-        if(f==0)return ans;
 
+        if(f==0)return ans;
         return -1;
     }
 };
