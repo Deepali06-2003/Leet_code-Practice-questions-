52630256cd2b0049ae05b791883f191015e8efb4
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-05 22:16:29 +0530

    Time: 0 ms (100%), Space: 8.6 MB (80.43%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
 void helper(int k , int n , vector<vector<int>>& res , vector<int>& curr , int curr_s , int x){
-    if(curr_s == 0){
-        if(k == curr.size()){
-            res.push_back(curr);
-        }
+    
+    if(curr_s ==0 ){
+        if(curr.size()==k)res.push_back(curr);
         return;
     }
 
-    for(int i =x;i<=9 ;i++){
-        if(i> curr_s)break;
+    for(int i = x;i<=9 ;i++){
+        if(i>curr_s)break;
 
         curr.push_back(i);
-        helper(k , n , res , curr , curr_s - i , i+1);
+        helper(k , n, res, curr, curr_s-i , i+1);
         curr.pop_back();
     }
 }
