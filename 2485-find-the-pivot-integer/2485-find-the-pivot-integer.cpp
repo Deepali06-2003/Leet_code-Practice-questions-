ca4d8d70bebac9d12b6ef9fa050bd897486ac4a1
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-06 23:01:07 +0530

    Time: 0 ms (100%), Space: 8 MB (69.73%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -4,19 +4,22 @@ public:
         if(n<=0)return -1;
         if(n==1)return 1;
 
-        int s = n * (n + 1) / 2;
-        int ls=0, rs=s;
+        int s=0;
+        for(int i=1;i<=n;i++)s += i;
+        
+        int ls =0 , rs =s;
+
         for(int i =1;i<=n;i++){
             if(i==1){
-                ls =0;
+                ls = 0;
                 rs = s-i;
             }
-            else{
-            ls = ls+ i-1;
-            rs = rs-i;
+            else {
+                ls = ls+i-1;
+                rs = rs-i;
             }
+
             if(ls == rs)return i;
-        }
-        return -1;
+        }return -1;
     }
 };
