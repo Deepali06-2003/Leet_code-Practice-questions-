039df2c738c43041e713ad62b58f855919e05786
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-07 19:00:55 +0530

    Time: 0 ms (100%), Space: 9.9 MB (77.2%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
 
 void toLower(string &s) {
-    transform(s.begin(), s.end(), s.begin(),[](unsigned char c){ return tolower(c); });
+    //transform(s.begin(), s.end(), s.begin(),[](unsigned char c){ return tolower(c); });
+    for(char c: s){
+        if(c>= 'A' && c<='Z') c = tolower(c);
+    }
 }
     bool isPalindrome(string s) {
         int l =0 , h = s.size()-1;
-        toLower(s);
-        cout<<s;
+        //toLower(s);
+        //cout<<s;
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
         while(l<h){
             if (!isalnum(s[l])) {
             l++;
