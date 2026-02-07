d228736f9fcc65d5685a3d01a4901c9743b3bd74
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-07 20:02:52 +0530

    Time: 5 ms (76.59%), Space: 19.6 MB (23.21%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -5,11 +5,11 @@ public:
     if (n <= 2) return n;
 
     int i = 2;
+    
     for (int j = 2; j < n; j++) {
-        if (nums[j] != nums[i - 2]) {
-            nums[i] = nums[j];
-            i++;
-        }
+        if (i >= 2 && nums[j] == nums[i - 2]) continue;
+        nums[i] = nums[j];
+        i++;
     }
     return i;
     }
