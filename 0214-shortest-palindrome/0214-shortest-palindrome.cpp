e4fb25dfdaafbdef7961a4d341d1e268d8784022
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-07 18:46:41 +0530

    Time: 20 ms (5.48%), Space: 12.1 MB (91.05%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -6,10 +6,10 @@ public:
         reverse(rev.begin(), rev.end());
 
         for(int i=0;i<s.size();i++){
-
-            if(memcmp(s.c_str() ,rev.c_str()+i, s.size()-i) ==0){
-                return rev.substr(0 , i)+s;
-            }
+                    // s substring start frm 0 ind,   rev substring start from i ind, length 
+                    // 
+            
+            if(memcmp(s.c_str() , rev.c_str()+i , s.size()-i ) ==0)return rev.substr(0 , i)+s;
         }return rev+s;
     }
 };
