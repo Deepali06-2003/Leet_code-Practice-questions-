d4fbbab2f792ec70ff7c1c420180b9bb983a9eb1
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-09 13:45:32 +0530

    Time: 143 ms (9.69%), Space: 149 MB (8.11%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
 
-bool Palindrome(string s , int l , int h){
-    while(l<=h){
 
-        if(s[l] != s[h])return false;
-
-        l++;h--;
-    }return true;
+bool isPalindrome(string s , int l , int h){
+    while(l<h){
+        if(s[l]!=s[h])return false;
+        l++; h--;
+    }
+    return true;
 }
-
-void helper(string s, vector<vector<string>>& res , vector<string>curr , int start){
-    if(start == s.size()){
+void helper(string s, vector<vector<string>>& res , vector<string> curr , int start){
+    
+    if(start ==s.size()){
         res.push_back(curr);
-        return;
+
     }
 
-    for(int i= start;i<s.size();i++){
-        
-        if(Palindrome(s, start , i)){
-            curr.push_back( s.substr(start , i-start+1) );
+    for(int i = start ;i<s.size();i++){
+        if(isPalindrome(s , start , i)){
+            curr.push_back(s.substr(start, i-start+1));
             helper(s , res , curr , i+1);
             curr.pop_back();
         }
-        
     }
+
 }
     vector<vector<string>> partition(string s) {
         vector<vector<string>>res;
