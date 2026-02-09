aa028a6eb20d06f74d38e003631dce10c616a1b1
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-09 22:28:58 +0530

    Time: 0 ms (100%), Space: 10.3 MB (15.1%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-int helper(vector<int>& nums , vector<vector<int>>& dp , int l , int h){
-    if(l>h)return 0;
-    if(dp[l][h] != INT_MIN)return dp[l][h];
-    if(l==h)return nums[l];
 
-    int i = nums[l] - helper(nums , dp , l+1 , h);
-    int j = nums[h] - helper(nums , dp , l , h-1);
+
+int solve(vector<int>& nums , int l , int h, vector<vector<int>>& dp){
+    if(l>h)return 0;
+    if(l == h)return nums[l];
+
+    if (dp[l][h] != INT_MIN) return dp[l][h];
+
+    int i = nums[l] - solve(nums , l+1 , h, dp);
+    int j = nums[h] - solve(nums , l , h-1, dp);
 
     return dp[l][h] = max(i , j);
 }
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp (n, vector<int>(n, INT_MIN));
-
-        return helper(nums , dp , 0 , n-1)>=0;
+        vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
+        return solve(nums , 0 , n-1 , dp) >=0;
     }
+
 };
