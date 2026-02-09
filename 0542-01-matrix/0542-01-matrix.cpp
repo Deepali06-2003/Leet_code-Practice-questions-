f44b670a0f7fc65aefcc35232d789796f8da71df
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-09 15:36:47 +0530

    Time: 10 ms (86.73%), Space: 34.5 MB (95.3%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,41 +3,34 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        queue<pair<int,int>>q;
 
-        for(int i=0;i<n ;i++){
+        queue<pair<int, int>>q;
+        for(int i=0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(mat[i][j] == 0) q.push({i, j});
-                else mat[i][j] = -1;
-                
+                if(mat[i][j]==0)q.push({i, j});
+                else mat[i][j]=-1;
             }
         }
-        vector<int>dx ={0,0,1,-1};
-        vector<int>dy={1,-1,0,0};
+
+        vector<int>dx = {0, 0, 1, -1};
+        vector<int>dy = {1, -1,0, 0};
 
         while(!q.empty()){
-            int size = q.size();
-            for(int i =0;i<size;i++){
-
-                pair<int,int>node = q.front();
+            int s=  q.size();
+            for(int i=0;i<s;i++){
+                pair<int, int>temp = q.front();
                 q.pop();
-                int r = node.first;
-                int c = node.second;
 
                 for(int j =0;j<4;j++){
+                    int nx = dx[j]+temp.first;
+                    int ny = dy[j]+temp.second;
 
-                    int x = r+ dx[j];
-                    int y = c+dy[j];
-
-                    if(y>=0 && y<m && x>=0 && x<n && mat[x][y] ==-1  ){
-                        
-                            mat[x][y] = mat[r][c] + 1;
-                            q.push({x, y});
-                        
+                    if(nx>=0 && nx<n && ny>=0 && ny<m && mat[nx][ny]==-1){
+                        mat[nx][ny]= mat[temp.first][temp.second]+1;
+                        q.push({nx, ny});
                     }
                 }
             }
-        }
-        return mat;
+        }return mat;
     }
 };
