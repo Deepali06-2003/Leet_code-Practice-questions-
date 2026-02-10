957e2e8699b4a59080dd5d8620e468eea5f4e8e8
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-10 14:11:30 +0530

    Time: 107 ms (25.28%), Space: 72 MB (5%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,7 +3,7 @@ public:
     bool checkInclusion(string s1, string s2) {
         if(s1.size()>s2.size())return false;
 
-        vector<int>s1_f(25,0);
+        vector<int>s1_f(26,0);
         for(int i=0;i<s1.size();i++){
             s1_f[s1[i]-'a']++;
         }
@@ -13,7 +13,7 @@ public:
 
             string x = s2.substr(l, r-l+1);
             
-            vector<int>s2_f(25,0);
+            vector<int>s2_f(26,0);
             for(int i=0;i<x.size();i++){
                 s2_f[x[i]-'a']++;
             }
