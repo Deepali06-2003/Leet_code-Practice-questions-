e94db61c087a3a94e059222e3b3b3daa7a335105
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-10 14:36:21 +0530

    Time: 1 ms (74.6%), Space: 10.1 MB (45.2%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -6,6 +6,7 @@ public:
         if(n>m)return false;
 
         vector<int>s1_f(26,0);
+        //cal frequency of s1;
         for(int i=0;i<s1.size();i++){
             s1_f[s1[i]-'a']++;
         }
@@ -14,9 +15,10 @@ public:
 
         int i=0, j=0;
         while(j<m){
-
+            //incrementing frequency of j;
             s2_f[s2[j]-'a']++;
 
+            //if window size increases by n(length of s1)----> decrease frequency of char i and i++;
             if((j-i+1)>n){
                 s2_f[s2[i]-'a']--;
                 i++;
