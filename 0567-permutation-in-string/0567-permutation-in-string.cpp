543ae5fcecbb55eb59b11b595012845a81a03ab5
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-10 14:05:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,15 +3,22 @@ public:
     bool checkInclusion(string s1, string s2) {
         if(s1.size()>s2.size())return false;
 
-        sort(s1.begin(), s1.end());
+        vector<int>s1_f(25,0);
+        for(int i=0;i<s1.size();i++){
+            s1_f[s1[i]-'a']++;
+        }
 
         int l=0, r = s1.size()-1;
         while(r<s2.size()){
 
             string x = s2.substr(l, r-l+1);
-            sort(x.begin(), x.end());
+            
+            vector<int>s2_f(25,0);
+            for(int i=0;i<x.size();i++){
+                s2_f[x[i]-'a']++;
+            }
 
-            if(x == s1)return true;
+            if(s2_f == s1_f)return true;
             l++;
             r++;
         }return false;
