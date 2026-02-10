c5d0f5e4b187a6b7c569301eebcc5289dfda0cd6
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-10 23:26:25 +0530

    Time: 0 ms (100%), Space: 10.1 MB (10.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,15 +5,12 @@ public:
         if(s.size() != t.size())return false;
 
        unordered_map<char, int>map;
-       for(char c: s){
+       for(char c:s){
         map[c]++;
        }
-
-       for(char c:t)map[c]--;
-
-       for(int i =0;i<map.size();i++){
-        if(map[i]!=0)return false;
-       }
-               return true;
+       for(char c:t){
+        map[c]--;
+        if(map[c]<0)return false;
+       }return true;
     }
 };
