fd24f49b099e0ce2c9ebdb3863ecf5cc98bdff5e
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-12 00:40:28 +0530

    Time: 20 ms (66.24%), Space: 17.5 MB (54.12%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        
+
         int n = nums.size();
-        sort(nums.begin(), nums.end());
         vector<vector<int>>res;
+        sort(nums.begin(), nums.end());
 
-        for(int i =0;i<n-3;i++){
-            if(i>0 && nums[i]== nums[i-1])continue;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
 
-            for(int j = i+1;j<n-2;j++){
+            for(int j =i+1;j<n;j++){
                 if(j>(i+1) && nums[j]==nums[j-1])continue;
 
-                int k = j+1, l = n-1;
-                    while(k<l){
-                        long long sum = nums[i]+nums[j];
-                        sum += nums[k];
-                        sum += nums[l];
+                int k = j+1, l=n-1;
 
-                        if(sum == target){
-                            vector<int>temp = {nums[i], nums[j], nums[k], nums[l]};
-                            res.push_back(temp);
+                while(k<l){
 
-                            k++;l--;
+                    long s = nums[i]+nums[j];
+                    s= s+nums[k];
+                    s= s+nums[l];
 
-                            while(k<l && nums[k]==nums[k-1])k++;
-                            while(k<l && nums[l] == nums[l+1])l--;
-                        }
-                        else if(sum > target)l--;
-                        else k++;
+                    if(s == target){
+                        vector<int>curr = {nums[i], nums[j], nums[k], nums[l]};
+                        res.push_back(curr);
+                        
+                        k++;
+                        l--;
 
+                        while(k<l && nums[k]==nums[k-1])k++;
+                        while(k<l && nums[l]==nums[l+1])l--;
                     }
-               
+                    else if(s>target)l--;
+                    else k++;
+
+                }
             }
-        }return res;
+        }
+
+        return res;
+
+
     }
 };
