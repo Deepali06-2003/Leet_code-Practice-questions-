9846350687b60057605e988a8e7d075dad1aaa95
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-11 23:39:54 +0530

    Time: 4 ms (13.3%), Space: 14.9 MB (83.41%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -6,16 +6,15 @@ public:
         vector<int>res(n, 0);
         
        int s=0;
-       for(int i=0;i<n;i++)s= s+nums[i];
+       for(int i :nums)s=s+i;
+       int ls=0 , rs=0;
 
-        int ls=0, rs=0;
-        for(int i =0;i<n;i++){
-            rs = s- nums[i]-ls;
-            res[i] = abs(ls-rs);
-
-            ls = ls+nums[i];
-            
-        }
+       for(int i=0;i<nums.size();i++){
+        rs = s -(nums[i]+ls);
+       
+        res[i]= abs(ls-rs);
+         ls = ls+ nums[i];
+       }
         
 
         
