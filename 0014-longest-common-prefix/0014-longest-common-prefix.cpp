bbee1867a65a5787f5d430a9c3350fc5ece186cb
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-12 00:27:32 +0530

    Time: 11 ms (5%), Space: 17.2 MB (5.02%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
 string helper(string ans , string s){
-    int n = ans.size();
-    int m = s.size();
-    int i =0, j=0;
-
+    int n = ans.size(), m = s.size();
+    int i=0, j=0;
     string str ="";
+
     while(i<n && j<m){
-        if(ans[i]==s[j]){
-            str = str+ ans[i];
+        if(ans[i]!=s[j])return str;
+        else{
+            str = str + ans[i];
             i++;
             j++;
         }
-        else return str;
     }
     return str;
 }
+
+
 
     string longestCommonPrefix(vector<string>& strs) {
         int n = strs.size();
