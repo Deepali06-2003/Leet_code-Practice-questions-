5f46c7f166719d741b8fdf3a0e3e16b5d0b6a2a1
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-11 23:56:19 +0530

    Time: 8 ms (17.12%), Space: 13.4 MB (78.7%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -4,13 +4,12 @@ public:
  
 
         vector<int>ans(2*n);
-       int i =0;
-       for(int j =0;j<n;j++){
-        ans[i] = nums[j];
-        ans[i+1] = nums[j+n];
-        i=i+2;
-       }
-        
+        int i =0;
+        for(int j =0 ;j<n;j++){
+            ans[i] = nums[j];
+            ans[i+1] = nums[j+n];
+            i=i+2;
+        }
         return ans;
     }
 };
