0f038ee7a55a13ed4f6534b57b566da74a114c64
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-12 16:13:57 +0530

    Time: 2 ms (70.87%), Space: 10 MB (45.44%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        
         int n = s1.size();
         int m = s2.size();
-        if(n>m)return false;
 
-        vector<int>s1_f(26,0);
-        //cal frequency of s1;
-        for(int i=0;i<s1.size();i++){
-            s1_f[s1[i]-'a']++;
-        }
-        
-        vector<int>s2_f(26,0);
+        vector<int>freq1(26, 0);
+        vector<int>freq2(26, 0);
+        for(int i=0; i<n ;i++)freq1[s1[i]-'a']++;
 
-        int i=0, j=0;
-        while(j<m){
-            //incrementing frequency of j;
-            s2_f[s2[j]-'a']++;
+        int l=0;
+        for(int r=0;r<m ;r++){
+            freq2[s2[r]-'a']++;
 
-            //if window size increases by n(length of s1)----> decrease frequency of char i and i++;
-            if((j-i+1)>n){
-                s2_f[s2[i]-'a']--;
-                i++;
+            if((r-l+1)>n){
+                freq2[s2[l]-'a']--;
+                l++;
             }
+            if(freq1 == freq2)return true;
 
-            if(s1_f == s2_f)return true;
-            j++;
-        }  
-        return false;
+        }return false;
     }
 };
