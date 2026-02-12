b74ec5628b0ae544de09b2da9064a8fe7aa1764a
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-12 12:18:38 +0530

    Time: 0 ms (100%), Space: 7.9 MB (99.95%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        if(n<=0)return -1;
-        if(n==1)return 1;
-
         int s=0;
-        for(int i=1;i<=n;i++)s += i;
-        
-        int ls =0 , rs =s;
+        for(int i =1; i<=n;i++) s= s+i;
 
-        for(int i =1;i<=n;i++){
-            if(i==1){
-                ls = 0;
-                rs = s-i;
-            }
-            else {
-                ls = ls+i-1;
-                rs = rs-i;
-            }
-
-            if(ls == rs)return i;
-        }return -1;
+        int ls=0, rs=0;
+        for(int i=1;i<=n;i++){
+            rs = s- (ls+i);
+            if(rs==ls)return i;
+            ls= ls+i;
+        }
+        return -1;
     }
 };
