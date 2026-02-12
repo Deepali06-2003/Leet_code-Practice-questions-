db9ce05fb4d253b015c81ffa653346b96683dd6b
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-12 14:12:43 +0530

    Time: 1 ms (45.74%), Space: 10.1 MB (36.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-
-void toLower(string &s) {
-    //transform(s.begin(), s.end(), s.begin(),[](unsigned char c){ return tolower(c); });
-    for(char c: s){
-        if(c>= 'A' && c<='Z') c = tolower(c);
-    }
-}
     bool isPalindrome(string s) {
-        int l =0 , h = s.size()-1;
-        //toLower(s);
-        //cout<<s;
-        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        int l=0, h = s.size()-1;
+        transform(s.begin(), s.end(), s.begin() ,:: tolower);
+
         while(l<h){
-            if (!isalnum(s[l])) {
-            l++;
-            continue;
+            if(!isalnum(s[l])){
+                l++;
+                continue;
             }
-            if (!isalnum(s[h])) {
+
+            if(!isalnum(s[h])){
                 h--;
                 continue;
             }
-            if(s[l]!=s[h])return false;
+            if(s[l]!= s[h])return false;
+
             l++;
             h--;
-        }
-        return true;
+        }return true;
     }
 };
