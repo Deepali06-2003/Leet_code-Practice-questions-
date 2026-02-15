c288814ad3936931d4580a498353c75f15478846
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-16 01:46:50 +0530

    Time: 4 ms (35.26%), Space: 19.9 MB (11.13%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -3,25 +3,23 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
         int n = nums.size();
-        unordered_map<int , int>map;
-        for(int i: nums)map[i]++;
+        unordered_map<int, int>map;
+        for(int i:nums)map[i]++;
 
         vector<vector<int>>freq(n+1);
-        for(auto ptr : map){
+        for(auto ptr: map){
             freq[ptr.second].push_back(ptr.first);
         }
 
         vector<int>res;
-        for(int i = n;i>=0;i--){
-            for(int j :freq[i]){
+        for(int i=freq.size()-1;i>=0;i--){
+            for(int j: freq[i]){
                 res.push_back(j);
                 k--;
 
                 if(k==0)return res;
             }
         }
-
-
         return res;
     }
 };
