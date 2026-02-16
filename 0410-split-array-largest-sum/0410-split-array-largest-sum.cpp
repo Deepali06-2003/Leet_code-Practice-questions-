fab03b894e2440c9d144a846218e1f309c6d0be7
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-16 14:57:27 +0530

    Time: 0 ms (100%), Space: 10.5 MB (27.38%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-int helper(vector<int>& nums, int pages){
+int helper(vector<int>& nums, int k, int m){
 
-    int s=1;
-    long long curr_s =0;
+    int c=1, curr_s =0;
+
     for(int i=0;i<nums.size();i++){
-
-        if((curr_s+nums[i])<=pages) curr_s = curr_s+nums[i];
+        if((curr_s+nums[i])<= m){
+            curr_s = curr_s+ nums[i];
+        }
         else{
-            s= s+1;
+            c= c+1;
             curr_s = nums[i];
+
         }
     }
-    return s;
+     return c;
 }
-
     int splitArray(vector<int>& nums, int k) {
         int n = nums.size();
-
-        if(k>n)return -1;
+        
         long long low = *max_element(nums.begin(), nums.end());
         long long high = accumulate(nums.begin(), nums.end(), 0);
 
         while(low<=high){
+
             long long mid = (low+high)/2;
 
-            int ans = helper(nums, mid);
-            if(ans>k) low = mid+1;
+            int x = helper(nums, k , mid);
+            if(x > k)low = mid+1;
             else high = mid-1;
+        }
 
-        }return low;
+        return low;
     }
 };
