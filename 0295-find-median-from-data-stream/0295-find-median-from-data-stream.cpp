9af138bef35a0671e05d7b4a453a7171b94783ed
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-17 11:34:34 +0530

    Time: 56 ms (88.93%), Space: 148.5 MB (63.36%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -8,11 +8,22 @@ public:
     
     void addNum(int num) {
         
-        max_heap.push(num);
-        min_heap.push(max_heap.top());
-        max_heap.pop();
+        // insert element -> condition
+        if(max_heap.empty() || num <= max_heap.top()){
+            max_heap.push(num);
+        }
+        else{
+            min_heap.push(num);
+        }
 
-        if(min_heap.size() > max_heap.size()){
+        //maxH.size()== minh.size() || maxH.size()==minH.size()+1
+        //updating size
+        if(max_heap.size() > min_heap.size()+1){
+            //pop ele from max_heap and push min_heap
+            min_heap.push(max_heap.top());
+            max_heap.pop();
+        }
+        else if (min_heap.size()> max_heap.size()){
             max_heap.push(min_heap.top());
             min_heap.pop();
         }
