49260004fa8db1dc9cb9b94f8ae14d3c83579823
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-18 12:52:18 +0530

    Time: 237 ms (76.59%), Space: 19.2 MB (59.9%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,77 +1,67 @@
 class Solution {
 public:
-    vector<string> res;
-    int n, m;
+int n , m;
+vector<string>res;
 
-    struct trieNode {
-        string word;
-        bool endWord;
-        trieNode* child[26];
+struct trieNode{
+    string word;
+    bool endW;
+    trieNode* child[26];
 
-        trieNode() {
-            word = "";
-            endWord = false;
-            for(int i = 0; i < 26; i++)
-                child[i] = NULL;
+    trieNode(){
+        word = "";
+        endW = false;
+        for(int i=0;i<26;i++) child[i]=NULL;
+    }
+};
+
+void insert(trieNode* root , string & w){
+    trieNode* t = root;
+
+    for(char c : w){
+        if(t->child[c-'a'] == NULL){
+            t->child[c-'a'] = new trieNode();
         }
-    };
-
-    void insert(trieNode* root, string& w) {
-        trieNode* t = root;
-
-        for(char c : w) {
-            if(t->child[c - 'a'] == NULL)
-                t->child[c - 'a'] = new trieNode();
-            t = t->child[c - 'a'];
-        }
-
-        t->word = w;
-        t->endWord = true;
+        t = t->child[c-'a'];
     }
 
-    void dfs(trieNode* node, vector<vector<char>>& board, int i, int j) {
+    t->word = w;
+    t->endW = true;
+}
 
-        if(i < 0 || j < 0 || i >= n || j >= m)
-            return;
+void dfs(vector<vector<char>>& board, trieNode* root , int i , int j){
 
-        char c = board[i][j];
-        if(c == '$' || node->child[c - 'a'] == NULL)
-            return;
+    if(i<0 || j<0 || i>=n || j>=m)return;
+    char c = board[i][j];
+    if(c == '$' || root->child[c-'a'] == NULL)return;
 
-        node = node->child[c - 'a'];
+    root = root->child[c-'a'];
 
-        if(node->endWord) {
-            res.push_back(node->word);
-            node->endWord = false;
-        }
-
-        board[i][j] = '$';
-
-        dfs(node, board, i + 1, j);
-        dfs(node, board, i - 1, j);
-        dfs(node, board, i, j + 1);
-        dfs(node, board, i, j - 1);
-
-        board[i][j] = c;
+    if(root->endW == true){
+        res.push_back(root->word);
+        root->endW = false;
     }
 
+    board[i][j]='$';
+
+    dfs(board, root, i+1 , j);
+    dfs(board, root, i -1, j);
+    dfs(board, root, i , j+1);
+    dfs(board, root, i , j-1);
+
+    board[i][j]= c;
+}
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-
-        res.clear();
-        n = board.size();
-        m = board[0].size();
+        n = board.size(), m = board[0].size();
 
         trieNode* root = new trieNode();
+        for(string w : words) insert(root, w);
 
-        for(string& w : words)
-            insert(root, w);
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                dfs(root, board, i, j);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                dfs(board, root, i , j);
             }
         }
-
         return res;
     }
 };
