0291f11e12fee3bd382ba490b74ca9e0ef45a0db
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-20 15:31:44 +0530

    Time: 13 ms (38.56%), Space: 35 MB (79.18%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        
         int n = maze.size();
         int m = maze[0].size();
-        int ans=0;
 
-        queue<pair<int , int>>q;
-        
-        q.push({entrance[0], entrance[1]}) ;
+        queue<pair<int, int>>q;
+        q.push({entrance[0], entrance[1]});
         maze[entrance[0]][entrance[1]] = '+';
 
         vector<int>dx = {0, 0, 1, -1};
-        vector<int>dy = {1, -1, 0, 0};
+        vector<int>dy = {1, -1, 0 , 0};
+        int c=1;
 
         while(!q.empty()){
-
             int s = q.size();
             for(int i=0;i<s;i++){
 
-                pair<int, int>temp = q.front();
+                pair<int, int> x = q.front();
                 q.pop();
-                
-                for(int j =0;j<4;j++){
-                    int nx = dx[j]+temp.first;
-                    int ny = dy[j]+temp.second;
 
-                    if(nx>=0 && nx<n && ny>=0 && ny<m && maze[nx][ny]=='.'){
-                        maze[nx][ny]='+';
+                for(int j =0;j<4 ;j++){
+
+                    int nx = x.first + dx[j];
+                    int ny = x.second + dy[j];
+                    if(nx>=0 && nx<n && ny>=0 && ny< m && maze[nx][ny]=='.'){
+                    if(nx== n-1 || nx==0 || ny==0 || ny == m-1){return c;}
+                        
                         q.push({nx, ny});
-
-                        if(nx ==0 || nx== n-1 || ny==0 || ny==m-1)return ans+1;
+                        maze[nx][ny]= '+';
                     }
                 }
-            }ans++;
-        }return -1;
-
+            }
+            c++;
+        }
+        return -1;
     }
 };
