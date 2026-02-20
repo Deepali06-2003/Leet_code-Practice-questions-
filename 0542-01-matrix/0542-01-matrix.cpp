642d9d7444cc2ab8e211269401c96d46c1b331ee
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-20 14:28:58 +0530

    Time: 32 ms (17.22%), Space: 34.5 MB (92.01%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        
         int n = mat.size();
         int m = mat[0].size();
 
         queue<pair<int, int>>q;
         for(int i=0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(mat[i][j]==0)q.push({i, j});
-                else mat[i][j]=-1;
+            for(int j=0;j<m;j++){
+                if(mat[i][j] == 0)q.push({i, j});
+                else mat[i][j]= -1;
             }
         }
 
-        vector<int>dx = {0, 0, 1, -1};
+        vector<int>dx = {0, 0, -1, 1};
         vector<int>dy = {1, -1,0, 0};
 
         while(!q.empty()){
-            int s=  q.size();
-            for(int i=0;i<s;i++){
-                pair<int, int>temp = q.front();
-                q.pop();
 
-                for(int j =0;j<4;j++){
-                    int nx = dx[j]+temp.first;
-                    int ny = dy[j]+temp.second;
+            pair<int, int>t = q.front();
+            q.pop();
 
-                    if(nx>=0 && nx<n && ny>=0 && ny<m && mat[nx][ny]==-1){
-                        mat[nx][ny]= mat[temp.first][temp.second]+1;
-                        q.push({nx, ny});
-                    }
+            for(int j =0;j<4;j++){
+                int nx = dx[j]+ t.first;
+                int ny = dy[j]+t.second;
+
+                if(nx>=0 && ny>=0 && ny<m && nx<n && mat[nx][ny]==-1){
+                    mat[nx][ny]= mat[t.first][t.second]+1;
+                    q.push({nx , ny});
                 }
             }
-        }return mat;
+        }
+
+        return mat;
     }
 };
