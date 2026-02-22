240966a410b96258aa4256e09125b90c999ac6ca
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-22 21:38:32 +0530

    Time: 6 ms (38.89%), Space: 14.3 MB (27.53%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-void helper(vector<int>& nums ,vector<int>& curr , vector<vector<int>>& res , int s, vector<bool>& used ){
+void helper(vector<int>& nums , vector<vector<int>>& res , vector<bool>& used, vector<int> curr, int s){
+
     if(curr.size() == nums.size()){
         res.push_back(curr);
         return;
     }
+    for(int i=0;i<nums.size();i++){
+        if(used[i])continue;
+        if(i>0 && nums[i] == nums[i-1] && used[i-1]==false)continue;
 
-    for(int i = 0;i<nums.size();i++){
-        if (used[i]) continue;
-            // skip duplicates: only use the first occurrence of duplicate numbers in this position
-        if (i > 0 && nums[i] == nums[i-1] && used[i-1] == true) continue;
-
-
-        used[i] = true;
+        used[i]=true;
         curr.push_back(nums[i]);
-        helper(nums , curr , res , i+1 , used);
+        helper(nums , res, used ,curr, i+1);
         curr.pop_back();
-
-        used[i] = false;
+        used[i]=false;
     }
 }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<int>curr;
         vector<vector<int>>res;
+        vector<int>curr;
+        vector<bool>used(nums.size(), false);
 
-        sort(nums.begin() , nums.end());
+        sort(nums.begin(), nums.end());
 
-        vector<bool>used(nums.size() , false);
-        helper(nums , curr, res , 0 , used);
+        helper(nums, res,used,  curr, 0);
         return res;
+
     }
 };
