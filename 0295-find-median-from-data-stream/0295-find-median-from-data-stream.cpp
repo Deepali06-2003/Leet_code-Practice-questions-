7973f1648a3358c393ba499886d62d0c7f9f7d0c
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-25 00:06:40 +0530

    Time: 82 ms (43.67%), Space: 148.5 MB (63.32%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,30 +1,33 @@
 class MedianFinder {
 public:
+
     priority_queue<int>max_heap;
     priority_queue<int, vector<int>, greater<int>> min_heap;
-
-
     MedianFinder() {}
     
     void addNum(int num) {
         
-        max_heap.push(num);
-        min_heap.push(max_heap.top());
-        max_heap.pop();
+        if(max_heap.empty() || num <= max_heap.top()){
+            max_heap.push(num);
+        }
+        else min_heap.push(num);
 
-        if(min_heap.size() > max_heap.size()){
+        if(max_heap.size() > min_heap.size()+1){
+            min_heap.push(max_heap.top());
+            max_heap.pop();
+        }
+        else if(min_heap.size() > max_heap.size()){
             max_heap.push(min_heap.top());
             min_heap.pop();
         }
-
     }
     
     double findMedian() {
-       
-       if(max_heap.size()>min_heap.size())return max_heap.top();
 
-       return (double)(max_heap.top() + min_heap.top())/2.0;
-
+        if(max_heap.size() > min_heap.size()){
+            return max_heap.top();
+        }
+        else return (double)(max_heap.top() + min_heap.top())/2.0;
     }
 };
 
@@ -34,3 +37,4 @@ public:
  * obj->addNum(num);
  * double param_2 = obj->findMedian();
  */
+
