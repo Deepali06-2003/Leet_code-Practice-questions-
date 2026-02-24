c7bef1e4e7e4b0c3adc120502126007ad07b79e3
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-25 00:21:11 +0530

    Time: 0 ms (100%), Space: 9.5 MB (18.68%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,5 +1,47 @@
 class Solution {
 public:
+unordered_map<char , vector<char>>mp;
+
+    void helper(string& digits , vector<string>& res, string curr, int s){
+       
+        if(s == digits.size()){
+            res.push_back(curr);
+            return;
+        }
+
+        
+            char j = digits[s];
+            vector<char> temp = mp[j];
+
+            for(char c : temp){
+                curr = curr + c;
+                helper(digits, res , curr , s+1);
+                curr.pop_back();
+            }
+        
+    }
+    vector<string> letterCombinations(string digits) {
+        mp['2'] = {'a','b','c'};
+        mp['3'] = {'d','e','f'};
+        mp['4'] = {'g','h','i'};
+        mp['5'] = {'j','k','l'};
+        mp['6'] = {'m','n','o'};
+        mp['7'] = {'p','q','r' ,'s'};
+        mp['8'] = {'t','u','v'};
+        mp['9'] = {'w','x','y','z'};
+
+        vector<string>res;
+        string curr;
+        if(digits.size() == 0)return res;
+
+        helper(digits, res , curr , 0);
+        return res;
+    }
+};
+
+
+
+
 unordered_map<int , vector<char>>mp;
 
 void helper(string digits,vector<string>& res , string curr, int start){
@@ -37,4 +79,3 @@ void helper(string digits,vector<string>& res , string curr, int start){
         helper(digits, res, curr, 0);
         return res;
     }
-};
