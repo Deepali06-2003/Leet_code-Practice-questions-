719669a2f5f9edbbc06d0d4400cd7f4684e49f85
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-24 23:20:46 +0530

    Time: 17 ms (95.85%), Space: 71.6 MB (53.81%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<pair<long long, pair<int,int>>> v;
+        
+        vector<pair<long long , pair<int, int>>>arr;
 
-        for(auto &p : points) {
+        for(auto& p : points){
             long long d = 1LL*p[0]*p[0] + 1LL*p[1]*p[1];
-            v.push_back({d, {p[0], p[1]}});
+            arr.push_back({d, {p[0], p[1]} });
         }
 
-        sort(v.begin(), v.end());
+        sort(arr.begin(), arr.end());
 
         vector<vector<int>>res;
-        for(int i = 0; i < k; i++) {
-            res.push_back({v[i].second.first,v[i].second.second});
+        for(int i =0;i<k ;i++){
+            res.push_back({arr[i].second.first , arr[i].second.second });
         }
 
-
-    return res;
+        return res;
     }
 };
