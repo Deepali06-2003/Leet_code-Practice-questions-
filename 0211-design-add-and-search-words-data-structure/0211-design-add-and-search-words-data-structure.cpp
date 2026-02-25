d26e894c79613c5096f8eced154c940ddd703806
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-25 09:58:16 +0530

    Time: 395 ms (56.65%), Space: 568.3 MB (38.15%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -1,52 +1,56 @@
 class WordDictionary {
 public:
-struct Node{
-    Node* child[26];
-    bool endWord;
+struct Trie{
+    Trie* child[26];
+    bool isEnd;
 
-    Node(){
-        for(int i=0;i<26;i++) child[i]= NULL;
-        endWord = false;
+    Trie(){
+        for(int i=0;i<26;i++)child[i] = NULL;
+        isEnd = false;
     }
 };
 
-Node* root;
-    WordDictionary() {root = new Node();}
+Trie * root;
+    WordDictionary() { root = new Trie(); }
     
     void addWord(string word) {
-        Node* t = root;
-        for(char c:word){
-            if(t->child[c-'a'] == NULL) t->child[c-'a'] = new Node();
-            t = t->child[c-'a'];
+        Trie* temp = root;
+        
+        for(char c: word){
+            if(temp->child[c-'a'] == NULL) temp->child[c-'a'] = new Trie();
+
+            temp = temp->child[c-'a'];
         }
-        t->endWord = true;
-    }
-
-    bool helper(Node* root , string word){
-        Node* t = root;
-
-        for(int i=0;i<word.size();i++){
-            char c = word[i]; 
-           
-            if(c == '.'){
-                for(int j=0;j<26;j++){
-                    if(t->child[j] != NULL){
-                        if(helper(t->child[j] , word.substr(i+1) ) == true ) return true;
-                    }
-                }
-                return false;
-            }
-
-            else if(t->child[c-'a'] == NULL)return false;
-
-            t = t->child[c-'a'];
-        }
-        if(t->endWord)return true;
-        return false;
+        temp->isEnd = true;
     }
     
+bool helper(Trie* root, string word){
+    Trie* temp = root;
+
+    for(int i=0 ;i< word.size();i++){
+        char c = word[i];
+
+        if(c == '.'){
+            for(int j =0; j<26 ;j++){
+                if(temp->child[j] != NULL){
+                   if( helper(temp->child[j], word.substr(i+1)) )return true;
+                }
+            }
+            return false;
+        }
+        else if(temp->child[c-'a'] == NULL)return false;
+
+        temp = temp->child[c-'a'];
+    }
+
+    if(temp->isEnd)return true;
+    return false;
+}
+
     bool search(string word) {
-        return helper(root, word);
+
+    Trie* temp = root;
+    return helper(root , word);  
     }
 };
 
