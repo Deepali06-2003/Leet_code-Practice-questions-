f621216a8ddd49ab5fcccb42e0b196c2a9a1a88e
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-26 09:49:38 +0530

    Time: 0 ms (100%), Space: 11.3 MB (81.91%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -5,26 +5,30 @@ public:
         int m = t.size();
 
         vector<int>freq(256, 0);
+        if(m >n)return "";
 
         for(char c: t) freq[c]++;
 
-        int s_i =-1 , le = INT_MAX, c=0;
-        int l=0, r=0;
+        int s_i = -1 , le = INT_MAX;
+        int c=0;
 
-        while(r<n){
+        int l =0 , r=0;
+        while(r < n){
 
-            if(freq[s[r]]>0)c++;
+            if(freq[s[r]] > 0)c++;
             freq[s[r]]--;
             r++;
 
-            while(c == m){
+            while(c==m){
                 if((r-l) < le){
                     s_i = l;
                     le = r-l;
                 }
+
                 freq[s[l]]++;
-                if(freq[s[l]] >0 )c--;
+                if(freq[s[l]]>0)c--;
                 l++;
+
             }
         }
         if(s_i == -1)return "";
