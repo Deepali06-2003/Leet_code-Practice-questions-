a25fa786dcf92aaa483dc830abae4954ed5270c6
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-26 09:56:50 +0530

    Time: 0 ms (100%), Space: 10.7 MB (92.55%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -4,23 +4,24 @@ public:
         int n = s.size();
         vector<int>freq(26, 0);
 
-        int max_f=0, max_l =0;
-        int l =0;
+        int max_l = 0 , max_freq = 0;
 
-        for(int r=0;r<n;r++){
-            freq[s[r]-'A']++;
-            max_f = max(max_f , freq[s[r]-'A']);
+        int l =0 ;
+        for(int r =0 ;r<n ;r++){
 
-            if( ((r-l+1)- max_f) > k){
-                freq[s[l]-'A']--;
+            freq[s[r] - 'A']++;
+            max_freq = max(max_freq , freq[s[r] - 'A']);
+
+            if( ((r-l+1) - max_freq )> k){
+                freq[s[l] - 'A']--;
                 l++;
             }
-
-            if( ((r-l+1)- max_f) <= k){
+            if( ((r-l+1) - max_freq )<= k){
                 max_l = max(max_l , (r-l+1));
             }
-        }
 
+   
+        }
         return max_l;
     }
 };
